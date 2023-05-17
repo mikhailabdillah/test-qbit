d0ead9fe95b8fe3435c95ddfb175b9e13a16c0a3
mikhail <mikhailabdillahtr@gmail.com>
2023-05-17 10:20:25 +0700

    feat: protected routes component added

---
+++ src/utils/ProtectedRoutes.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import type { RootState } from '@/store'
+
+const ProtectedRoute = ({ children }: { children?: React.ReactNode }) => {
+  const user = useSelector((state: RootState) => state.auth.user)
+  if (!user) {
+    return <Navigate to={'/login'} replace />
+  }
+  return <>{children}</>
+}
+
+export default ProtectedRoute

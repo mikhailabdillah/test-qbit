35d976ee998fd6dfb9ea765af9b86a45ee582338
mikhail <mikhailabdillahtr@gmail.com>
2023-05-18 01:13:36 +0700

    feat(navbar): set a static menu + conditional additional menu

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,6 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import cn from 'clsx'
+import { useSelector } from 'react-redux'
+import { RootState, useAppDispatch } from '@/store'
+
+const menu = [
+  {
+    text: 'Home',
+    href: '/',
+    icon: 'ti-home',
+  },
+  {
+    text: 'About',
+    href: '/about',
+    icon: 'ti-user',
+  },
+  {
+    text: 'Posting',
+    href: '/posting',
+    icon: 'ti-book',
+  },
+]
 
 const Navbar = () => {
+  const location = useLocation()
+  const user = useSelector((state: RootState) => state.auth.user)
+  const dispatch = useAppDispatch()
+
   return (
     <header className="minibar">
       <div className="header">
@@ -8,30 +33,43 @@ const Navbar = () => {
       </div>
       <div className="content">
         <ul className="main-menu">
-          <li className="menu-item active">
-            <Link to="/" className="menu-link">
-              <span className="icon ti-home"></span>
-              <span className="caption">Home</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link to="/about" className="menu-link">
-              <span className="icon ti-user"></span>
-              <span className="caption">About</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link to="/posting" className="menu-link">
-              <span className="icon ti-book"></span>
-              <span className="caption">Posting</span>
-            </Link>
-          </li>
-          <li className="menu-item">
-            <Link to="/login" className="menu-link">
-              <span className="icon ti-shift-right"></span>
-              <span className="caption">Login</span>
-            </Link>
-          </li>
+          {menu.map((data) => (
+            <li
+              key={data.text}
+              className={cn(
+                'menu-item',
+                location.pathname === data.href && 'active'
+              )}
+            >
+              <Link to={data.href} className="menu-link">
+                <span className={cn('icon', data.icon)}></span>
+                <span className="caption">{data.text}</span>
+              </Link>
+            </li>
+          ))}
+          {!user.isLogged ? (
+            <li
+              className={cn(
+                'menu-item',
+                location.pathname === '/login' && 'active'
+              )}
+            >
+              <Link to="/login" className="menu-link">
+                <span className="icon ti-shift-right"></span>
+                <span className="caption">Login</span>
+              </Link>
+            </li>
+          ) : (
+            <li className="menu-item">
+              <button
+                className="menu-link"
+                onClick={() => dispatch({ type: 'auth/logout' })}
+              >
+                <span className="icon ti-shift-left"></span>
+                <span className="caption">Logout</span>
+              </button>
+            </li>
+          )}
         </ul>
       </div>
     </header>

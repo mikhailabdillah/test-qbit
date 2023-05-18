b5ca0beed8c02526fca8f92253e4d62844ce4b7c
mikhail <mikhailabdillahtr@gmail.com>
2023-05-18 20:51:47 +0700

    refactor: login page component, split form into independent component

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,20 +1,12 @@
+import { LoginForm } from '@/components/Form'
 import Layout from '@/components/Layout/Layout'
-import { RootState, useAppDispatch } from '@/store'
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  Heading,
-  Input,
-} from '@chakra-ui/react'
-import { Formik, Form } from 'formik'
+import { RootState } from '@/store'
+import { Box, Heading } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
-import { Navigate, redirect } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 const LoginPage = () => {
   const user = useSelector((state: RootState) => state.auth.user)
-  const dispatch = useAppDispatch()
   if (user.isLogged) {
     return <Navigate to={'/'} replace />
   }
@@ -25,53 +17,7 @@ const LoginPage = () => {
         <Heading as={'h1'} size={'4xl'} textAlign={'center'}>
           Login
         </Heading>
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-          }}
-          onSubmit={(values) => {
-            if (values.username === 'admin' && values.password === 'qbit123') {
-              console.log(values)
-              dispatch({
-                type: 'auth/setAuth',
-                payload: { isLogged: true, token: '' },
-              })
-              redirect('/')
-            }
-            throw new Error(
-              'Failed to login, Please check username & password!'
-            )
-          }}
-        >
-          {({ values, handleChange, handleSubmit }) => (
-            <Form>
-              <Container maxW={'xl'} py={10}>
-                <FormControl mb={4}>
-                  <Input
-                    type="text"
-                    value={values.username}
-                    onChange={handleChange('username')}
-                    placeholder="Username"
-                  />
-                </FormControl>
-                <FormControl mb={4}>
-                  <Input
-                    type="password"
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    placeholder="Password"
-                  />
-                </FormControl>
-                <FormControl>
-                  <Button type="submit" onClick={() => handleSubmit()}>
-                    Sign In
-                  </Button>
-                </FormControl>
-              </Container>
-            </Form>
-          )}
-        </Formik>
+        <LoginForm />
       </Box>
     </Layout>
   )
